af18b57b7572e39c64b33d68a9cb783cf25f8c40
Shreya Upadhyay <upadhyayshreya1807@gmail.com>
2024-05-30 00:42:32 +0530

    Initial commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLAS_SERVER_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() =>{
     console.log("connection successful");
